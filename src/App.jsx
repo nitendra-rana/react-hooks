6c146f276120afd2c0928fc81136e7a3161d5510
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-05 23:37:11 +0530

    updated hook and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import Navigation from "./Header/Navigation";
+import UseCallback from "./UseCallback/UseCallback";
 import UseContext from "./UseCotext/UseContext";
 import UseEffect from "./useEffect/UseEffect";
 import UseImperative from "./UseImperative/UseImperative";
 import UseLayoutEffect from "./UseLayoutEffect/UseLayoutEffect";
+import UseMemo from "./UseMemo/UseMemo";
 import UseReducer from "./UseReducer/UseReducer";
 import UseRef from "./UseRef/UseRef";
 import Usestate from "./useState/Usestate";
-
+import { Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
@@ -15,13 +18,19 @@ function App() {
         <Navigation />
       </div>
       <div className="items">
-        <UseContext/>
-        <Usestate />
-        <UseReducer />
-        <UseEffect />
-        <UseRef />
-        <UseLayoutEffect/>
-        <UseImperative/>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/usestate" element={<Usestate />} />
+          <Route path="/usereducer" element={<UseReducer />} />
+          <Route path="/useeffect" element={<UseEffect />} />
+          <Route path="/useref" element={<UseRef />} />
+          <Route path="/uselayouteffect" element={<UseLayoutEffect />} />
+          <Route path="/useImperative" element={<UseImperative />} />
+          <Route path="/usecontext" element={<UseContext />} />
+          <Route path="/useMemo" element={<UseMemo />} />
+          <Route path="/usecallback" element={<UseCallback />} />
+          <Route path="*" element={<UseCallback />} />
+        </Routes>
       </div>
     </div>
   );
--- src/Header/Navigation.jsx
+++ src/Header/Navigation.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import "./Navigation.css"
-
+import { Link } from 'react-router-dom'
 
 
 const Navigation = () => {
   return (
     <div className='Nav'>
-      <div className="logo"><h3>React-hooks</h3></div>
-      <div className='item'>useState</div>
-      <div className='item'>useReducer</div>
-      <div className='item'>useEffect</div>
-      <div className='item'>useLayout</div>
-      <div className='item'>useRef</div>
-      <div className='item'>useContext</div>
-      <div className='item'>useMemo</div>
+      <Link to = "/"><button className="logo"><h3>React-hooks</h3></button></Link>
+      <Link to ="/usestate"><button className='item'>useState</button></Link>
+      <Link to ="/usereducer"><button className='item'>useReducer</button></Link>
+      <Link to ="/useeffect"><button className='item'>useEffect</button></Link>
+      <Link to ="/uselayouteffect"><button className='item'>useLayoutEffect</button></Link>
+     <Link to ="/useref"> <button className='item'>useRef</button></Link>
+      <Link to ="/usecontext"><button className='item'>useContext</button></Link>
+      <Link to ="/usememo"><button className='item'>useMemo</button></Link>
+      <Link to ="/usecallback"><button className="item">useCallback</button></Link>
+      <Link to ="/useimperative"><button className="item">useImperative</button></Link>
     </div>
   )
 }
